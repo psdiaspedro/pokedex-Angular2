a5e0d1588928df71208014a5e85942d6d4431735
Pedro Dias <psdias.pedro@gmail.com>
2022-06-09 17:31:04 -0300

    doing api

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from "src/app/service/poke-api.service"
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+	constructor(
+		private PokeApiService: PokeApiService
+	) { }
+
+	ngOnInit(): void {
+		this.PokeApiService.apiListAllPokemons.subscribe(
+			res => console.log(res)
+		);
+	}
+
+}
